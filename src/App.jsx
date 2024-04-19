cfa639885901739474c0ea952c2409a921454eac
Noelia <sharimanoe@gmail.com>
2024-04-19 17:36:16 +0200

    lab solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
 
+import HomePage from "./pages/HomePage";
+// import AboutPage from "./pages/AboutPage";
+// import ProjectsPage from "./pages/ProjectsPage";
+// import ErrorPage from "./pages/ErrorPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
+
+      {/*   Add <Route /> components between <Routes> and </Routes>   */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/students/:studentId" element={<StudentDetailsPage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
+      </Routes>
 
       <div className="pages">
         <HomePage />
@@ -18,7 +28,6 @@ function App() {
 
         <UserProfilePage />
       </div>
-
     </div>
   );
 }
